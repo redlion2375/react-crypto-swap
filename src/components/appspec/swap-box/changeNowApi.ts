e685af02f7e95fe9d5eedac2708e4e8ed6d5ddd6
GR7 <wintermountain2375@gmail.com>
2023-05-21 12:40:11 +0900

    add: api 4 crt exchge

--- src/components/appspec/swap-box/changeNowApi.ts
+++ src/components/appspec/swap-box/changeNowApi.ts
@@ -60,3 +60,53 @@ export const getEstimatedRangeFixedRate = async (from_to: String): Promise<Axios
     }
 }
 
+//create fixed rate exchange
+//The API endpoint creates a transaction, generates an address for sending funds and returns transaction attributes.
+//successful response fields: {id, payinAddress, payoutAddress, payoutExtrald, fromCurrency, toCurrency, result, refundAddress, refundExtrald, payoutExtraldName, validUntil, rateId}
+/**
+ * 
+ * @param requestObj
+ * @type 
+ * {
+     "from": "btc",
+     "to": "xlm",
+     "address": "GAM6Y7R5LKBYOC5VCF3L3B24EMM2IA5S7KTWTR65G65N7BJQRV32OGFB",
+     "amount": "12.0645",
+     "extraId": "123456789",
+     "refundAddress": "1Nh7uHdvY6fNwtQtM1G5EZAFPLC33B59rB",
+     "refundExtraId": "",
+     "userId": "",
+     "payload": "",
+     "contactEmail": ""
+}
+ */
+export const createFixedRateExchange = async (from_to: String, requestObj: Object): Promise<AxiosResponse<any>> => {
+    const apiKey = personInfo.apiKey;
+    const apiUrl = `https://api.changenow.io/v1/exchange-range/fixed-rate/${from_to}?api_key=${apiKey}&useRateId=true`;
+
+    try {
+        const response = await axios.post(apiUrl, {
+            ...requestObj
+        })
+        return response;
+    } catch (error: any) {
+        throw new Error(`Error fetching currencies: ${error.message}`);
+    }
+}
+
+
+//create fixed rate exchange reverse
+//The API endpoint creates a transaction, generates an address for sending funds and returns transaction attributes.
+//successful response fields: {}
+
+export const createReverseFixedRateExchange  = async (from_to: String): Promise<AxiosResponse<any>> => {
+    const apiKey = personInfo.apiKey;
+    const apiUrl = `https://api.changenow.io/v1/exchange-range/fixed-rate/${from_to}?api_key=${apiKey}&useRateId=true`;
+
+    try {
+        const response = await axios.get(apiUrl)
+        return response;
+    } catch (error: any) {
+        throw new Error(`Error fetching currencies: ${error.message}`);
+    }
+}
